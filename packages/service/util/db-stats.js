5458ab4664e6596a2701e870c26356ae86727d8a
Hudson Silva Borges <hudsonsilbor@gmail.com>
2020-04-23 13:42:25 -0400

    feat: provides a better database overview script

---
+++ packages/service/util/db-stats.js
@@ -0,0 +1,45 @@
+/*
+ *  Author: Hudson S. Borges
+ */
+global.Promise = require('bluebird');
+
+require('dotenv').config();
+require('pretty-error').start();
+
+const numeral = require('numeral');
+const chalk = require('chalk');
+const { table } = require('table');
+const connection = require('../modules/connection.js');
+
+Promise.resolve()
+  .then(() => connection.connect())
+  .then(() =>
+    connection.db
+      .listCollections()
+      .toArray()
+      .map((c) => c.name)
+  )
+  .then((names) =>
+    Promise.mapSeries(names, async (name) => {
+      const stats = await connection.db.collection(name).stats();
+      return [
+        name,
+        numeral(stats.count).format('0.[00]a'),
+        numeral(stats.storageSize).format('0.0b'),
+        numeral(stats.nindexes).format('0,0'),
+        numeral(stats.totalIndexSize).format('0.0b')
+      ];
+    })
+  )
+  .then((data) =>
+    [
+      ['Collection', 'Documents', 'Storage Size', 'Num. Indexes', 'Indexes Size'].map((c) =>
+        chalk.bold(c)
+      )
+    ].concat(data)
+  )
+  .then((data) =>
+    table(data, { columnDefault: { alignment: 'right' }, columns: { 0: { alignment: 'left' } } })
+  )
+  .then((data) => console.log(data))
+  .finally(() => connection.disconnect());
