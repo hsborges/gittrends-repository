cfbc6b6cdf6935d95b6719fc1fd48b9164a8980f
Hudson Silva Borges <hudsonsilbor@gmail.com>
2020-07-20 18:42:27 -0400

    feat: add an ending line with totals

--- packages/service/util/db-stats.js
+++ packages/service/util/db-stats.js
@@ -20,25 +20,35 @@ Promise.resolve()
       .toArray()
       .map((c) => c.name)
   )
-  .then((names) =>
-    Promise.mapSeries(names, async (name) => {
+  .then((names) => {
+    return Promise.mapSeries(names, async (name) => {
       const stats = await connection.db.collection(name).stats();
-      return [
-        name,
-        numeral(stats.count).format('0.[00]a'),
-        numeral(stats.storageSize).format('0.0b'),
-        numeral(stats.nindexes).format('0,0'),
-        numeral(stats.totalIndexSize).format('0.0b')
-      ];
-    })
-  )
-  .then((data) =>
-    [
-      ['Collection', 'Documents', 'Storage Size', 'Num. Indexes', 'Indexes Size'].map((c) =>
-        chalk.bold(c)
-      )
-    ].concat(data.sort())
-  )
+      return [name, stats.count, stats.storageSize, stats.nindexes, stats.totalIndexSize];
+    }).then((data) =>
+      [
+        ...data.sort(),
+        data.reduce((m, d) => [
+          '',
+          d[1] + (m[1] || 0),
+          d[2] + (m[2] || 0),
+          d[3] + (m[3] || 0),
+          d[4] + (m[4] || 0)
+        ])
+      ].map((row) => [
+        row[0],
+        numeral(row[1]).format('0.[00]a'),
+        numeral(row[2]).format('0.0b'),
+        numeral(row[3]).format('0,0'),
+        numeral(row[4]).format('0.0b')
+      ])
+    );
+  })
+  .then((data) => [
+    ['Collection', 'Documents', 'Storage Size', 'Num. Indexes', 'Indexes Size'].map((c) =>
+      chalk.bold(c)
+    ),
+    ...data
+  ])
   .then((data) =>
     table(data, { columnDefault: { alignment: 'right' }, columns: { 0: { alignment: 'left' } } })
   )
