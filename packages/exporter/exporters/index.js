9fdaaa9b4363afdaafb42fa5634c7ca7799e2e73
Hudson S. Borges <hudsonsilbor@gmail.com>
2020-11-17 19:46:59 -0400

    refactor: exporter is no longer necessary

--- packages/exporter/exporters/index.js
+++
@@ -1,8 +0,0 @@
-/*
- *  Author: Hudson S. Borges
- */
-module.exports.repository = require('./repository');
-module.exports.stargazers = require('./stargazers');
-module.exports.user = require('./user');
-module.exports.users = require('./users');
-module.exports.watchers = require('./watchers');
