c394d4990db4203e3763ee93bb7981b21e9708f3
Hudson S. Borges <hudsonsilbor@gmail.com>
2020-11-29 16:13:22 -0400

    fix: minor

--- scripts/prepare-ubuntu.sh
+++ scripts/prepare-ubuntu.sh
@@ -1,12 +1,11 @@
-sudo apt update
+sudo apt update && sudo apt dist-upgrade -y
 sudo apt install -y curl tmux docker docker-compose
-sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 
 curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt install -y nodejs
 
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install -y yarn
+sudo apt update && sudo apt install -y yarn
