26f8f35f3c8f94e61f0c0ac647e13e6bf7a2bf94
Hudson S. Borges <hudsonsilbor@gmail.com>
2020-11-29 15:24:30 -0400

    fix: fix queue

--- packages/service/util/queue-board.js
+++ packages/service/util/queue-board.js
@@ -7,28 +7,24 @@ require('@hsborges/env-config');
 // Mandatory import of queue library.
 const Bee = require('bee-queue');
 const Arena = require('bull-arena');
-const { capitalize } = require('lodash');
-
-// constants
-const {
-  config: { resources }
-} = require('../package.json');
 
 // queues
 Arena(
   {
     Bee,
-    queues: resources.map((_resource) => ({
-      name: `updates:${_resource}`,
-      hostId: capitalize(_resource),
-      redis: {
-        host: process.env.GITTRENDS_REDIS_HOST || 'localhost',
-        port: parseInt(process.env.GITTRENDS_REDIS_PORT || 6379, 10),
-        db: parseInt(process.env.GITTRENDS_REDIS_DB || 0, 10)
-      },
-      type: 'bee',
-      prefix: 'bq'
-    }))
+    queues: [
+      {
+        name: 'Updates',
+        hostId: 'Updates',
+        redis: {
+          host: process.env.GITTRENDS_REDIS_HOST || 'localhost',
+          port: parseInt(process.env.GITTRENDS_REDIS_PORT || 6379, 10),
+          db: parseInt(process.env.GITTRENDS_REDIS_DB || 0, 10)
+        },
+        type: 'bee',
+        prefix: 'bq'
+      }
+    ]
   },
   { port: process.env.GITTRENDS_QUEUE_BOARD_PORT }
 );
