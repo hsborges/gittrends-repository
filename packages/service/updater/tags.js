4b41502ead0950d679a3f115397e1bb929a3979d
Hudson S. Borges <hudsonsilbor@gmail.com>
2020-12-01 20:27:50 -0400

    fix: minor

--- packages/service/updater/tags.js
+++ packages/service/updater/tags.js
@@ -6,27 +6,35 @@ const { knex } = require('@gittrends/database-config');
 const dao = require('./helper/dao');
 const getTags = require('../github/graphql/repositories/tags.js');
 
+const BATCH_SIZE = parseInt(process.env.GITTRENDS_BATCH_SIZE || 500, 10);
+
 /* exports */
 module.exports = async function (repositoryId) {
   const path = { id: repositoryId, resource: 'tags' };
   const metadata = await dao.metadata.find({ ...path, key: 'lastCursor' }).first();
 
-  const lastCursor = metadata && metadata.value;
-  const result = await getTags(repositoryId, { lastCursor });
+  let lastCursor = metadata && metadata.value;
 
-  return knex.transaction(async (trx) => {
-    await Promise.all([
-      dao.actors.insert(result.users, trx),
-      dao.commits.insert(result.commits, trx),
-      dao.tags.insert(
-        result.tags.map((t) => ({ repository: repositoryId, ...t })),
-        trx
-      )
-    ]);
+  for (let hasMore = true; hasMore; ) {
+    const result = await getTags(repositoryId, { lastCursor, max: BATCH_SIZE });
 
-    return dao.metadata.upsert([
-      { ...path, key: 'updatedAt', value: new Date().toISOString() },
-      { ...path, key: 'lastCursor', value: result.endCursor || lastCursor }
-    ]);
-  });
+    await knex.transaction(async (trx) => {
+      await Promise.all([
+        dao.actors.insert(result.users, trx),
+        dao.commits.insert(result.commits, trx),
+        dao.tags.insert(
+          result.tags.map((t) => ({ repository: repositoryId, ...t })),
+          trx
+        ),
+        dao.metadata.upsert(
+          { ...path, key: 'lastCursor', value: (lastCursor = result.endCursor || lastCursor) },
+          trx
+        )
+      ]);
+    });
+
+    hasMore = result.hasNextPage;
+  }
+
+  return dao.metadata.upsert({ ...path, key: 'updatedAt', value: new Date().toISOString() });
 };
