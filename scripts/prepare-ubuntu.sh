2ed209693d52e154c3de61e37830fd92605885b6
Hudson S. Borges <hudsonsilbor@gmail.com>
2020-12-06 20:16:43 -0400

    fix: fix script commands

--- scripts/prepare-ubuntu.sh
+++ scripts/prepare-ubuntu.sh
@@ -12,12 +12,11 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.lis
 apt update && apt install -y yarn
 
 # install and configure docker
-apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update && apt-get install docker-ce docker-ce-cli containerd.io
+apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
 usermod -aG docker $USER
-newgrp docker
 systemctl enable docker
 systemctl start docker
 
