c3ee8b480241ac3e9098930c3d63d97e5203ac4a
Hudson S. Borges <hudsonsilbor@gmail.com>
2020-12-08 23:52:22 -0400

    feat: add zsh scripts to ubuntu installs script

--- scripts/prepare-ubuntu.sh
+++ scripts/prepare-ubuntu.sh
@@ -3,6 +3,20 @@
 apt update && apt dist-upgrade -y
 apt install -y curl tmux postgresql-client
 
+# install zsh and some useful plugins
+apt install -y zsh
+chsh -s $(which zsh)
+zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+yes y | sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
+echo "ZSH_THEME=\"spaceship\"" >> ~/.zshrc
+echo "zinit light zdharma/fast-syntax-highlighting" >>  ~/.zshrc
+echo "zinit light zsh-users/zsh-autosuggestions" >>  ~/.zshrc
+echo "zinit light zsh-users/zsh-completions" >>  ~/.zshrc
+
+
 # install nodejs and yarn
 curl -sL https://deb.nodesource.com/setup_15.x | bash -
 apt install -y nodejs
@@ -10,9 +24,6 @@ apt install -y nodejs
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt update && apt install -y yarn
-
-# force npx update
-npm install -g --force npx
 
 # install and configure docker
 apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
