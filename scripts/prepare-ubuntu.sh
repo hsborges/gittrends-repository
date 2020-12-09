3af55fe2c36ba5a46fc7124d414abd54359f4d1e
Hudson S. Borges <hudsonsilbor@gmail.com>
2020-12-09 00:30:35 -0400

    fix: minor

--- scripts/prepare-ubuntu.sh
+++ scripts/prepare-ubuntu.sh
@@ -29,9 +29,10 @@ apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io
-usermod -aG docker $USER
 systemctl enable docker
-systemctl start docker
+systemctl restart docker
+usermod -aG docker $USER
+newgrp docker
 
 curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
