c84756268569dbda9658d24f4c1b9aad1b3b5451
Hudson S. Borges <hudsonsilbor@gmail.com>
2021-01-14 11:21:17 -0400

    refactor: rewrite website in react

--- packages/website/components/Love.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="footer flex justify-center font-bold pb-2 lg:pt-0 leading-none">
-    made with<i class="fas fa-heart px-1"></i> by
-    <a class="pl-1" href="https://github.com/hsborges" target="_blank"> hsborges </a>
-  </div>
-</template>
