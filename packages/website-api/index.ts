b6540fcd584a5fa206a8e73a23a772bd28d62d75
Hudson Silva Borges <hudsonsilbor@gmail.com>
2021-01-25 14:27:30 -0300

    refactor: minorr

--- packages/website-api/index.ts
+++ packages/website-api/index.ts
@@ -1,11 +1,10 @@
 import application from './app';
 
 const fastify = application();
-const PORT: number = parseInt(process.env.PORT ?? '3000', 10);
 
 const start = async (): Promise<void> => {
   try {
-    await fastify.listen(PORT, '0.0.0.0');
+    await fastify.listen(process.env.PORT ?? 3000, '0.0.0.0');
   } catch (error) {
     fastify.log.error(error);
     process.exit(1);
