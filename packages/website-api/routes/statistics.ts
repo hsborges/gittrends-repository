70f7b1e1b417710faac933e5802f827db5e452d5
Hudson Silva Borges <hudsonsilbor@gmail.com>
2021-02-03 21:24:06 -0400

    minor

--- packages/website-api/routes/statistics.ts
+++ packages/website-api/routes/statistics.ts
@@ -1,13 +1,14 @@
 import { FastifyInstance } from 'fastify';
 import knex, * as Models from '@gittrends/database-config';
-import Model from '@gittrends/database-config/dist/models';
 
 export default async function (fastify: FastifyInstance): Promise<void> {
-  const query = (Model: Model) =>
-    Model.query()
+  async function query<T extends Models.Model>(model: T) {
+    return model
+      .query()
       .select(knex.raw('count(*)::integer as count'))
       .first()
       .then(({ count }) => count);
+  }
 
   fastify.get('/statistics', async function (request, reply) {
     const [repositories, users, stargazers, tags] = await Promise.all([
