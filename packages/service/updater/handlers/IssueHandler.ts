6c6845948356b2c4d675c14e6ea0c2ef53d72022
Hudson S. Borges <hudsonsilbor@gmail.com>
2021-02-15 23:15:41 -0400

    minor

--- packages/service/updater/handlers/IssueHandler.ts
+++ packages/service/updater/handlers/IssueHandler.ts
@@ -46,10 +46,17 @@ type TReactableMetadata = {
 
 type TResource = 'issues' | 'pull_requests';
 
+enum Stages {
+  GET_ISSUES_LIST,
+  GET_ISSUES_DETAILS,
+  GET_REACTIONS
+}
+
 export default class RepositoryIssuesHander extends AbstractRepositoryHandler {
   private resource: TResource;
   private issues: { items: TIssueMetadata[]; hasNextPage: boolean; endCursor?: string };
   private reactions?: TReactableMetadata[];
+  private currentStage?: Stages;
 
   constructor(id: string, alias?: string, type: TResource = 'issues') {
     super(id, alias, type);
@@ -61,9 +68,11 @@ export default class RepositoryIssuesHander extends AbstractRepositoryHandler {
   async component(): Promise<RepositoryComponent | Component[]> {
     if (
       this.issues.hasNextPage &&
-      this.getPendingIssues().length === 0 &&
-      this.getPendingReactables().length === 0
+      this.pendingIssues.length === 0 &&
+      this.pendingReactables.length === 0
     ) {
+      this.currentStage = Stages.GET_ISSUES_LIST;
+
       if (!this.issues.endCursor) {
         this.issues.endCursor = await Repository.collection
           .findOne({ _id: this.meta.id }, { projection: { _metadata: 1 } })
@@ -82,10 +91,11 @@ export default class RepositoryIssuesHander extends AbstractRepositoryHandler {
       });
     }
 
-    const pendingIssues = this.getPendingIssues();
-    if (pendingIssues.length > 0) {
+    if (this.pendingIssues.length > 0) {
+      this.currentStage = Stages.GET_ISSUES_DETAILS;
+
       return Promise.all(
-        pendingIssues.map(async (issue) => {
+        this.pendingIssues.map(async (issue) => {
           if (!issue.timeline.endCursor) {
             issue.timeline.endCursor = await Issue.collection
               .findOne({ _id: issue.data.id }, { projection: { _metadata: 1 } })
@@ -114,9 +124,10 @@ export default class RepositoryIssuesHander extends AbstractRepositoryHandler {
       );
     }
 
-    const pendingReactables = this.getPendingReactables();
-    if (pendingReactables.length > 0) {
-      return pendingReactables.map((reactable) =>
+    if (this.pendingReactables.length > 0) {
+      this.currentStage = Stages.GET_REACTIONS;
+
+      return this.pendingReactables.map((reactable) =>
         reactable.component.includeReactions(reactable.hasNextPage, {
           first: 50,
           after: reactable.endCursor
@@ -124,7 +135,7 @@ export default class RepositoryIssuesHander extends AbstractRepositoryHandler {
       );
     }
 
-    return [];
+    throw new ResourceUpdateError('Invalid condition reached!');
   }
 
   async update(response: TObject, session?: ClientSession): Promise<void> {
@@ -136,156 +147,158 @@ export default class RepositoryIssuesHander extends AbstractRepositoryHandler {
   private async _update(response: TObject, session?: ClientSession): Promise<void> {
     if (this.isDone()) return;
 
-    if (this.issues.items.length === 0) {
-      const data = response[super.alias as string];
+    switch (this.currentStage) {
+      case Stages.GET_ISSUES_LIST: {
+        const data = response[super.alias as string];
 
-      this.issues.items = get(data, `${this.resource}.nodes`, []).map((issue: TObject) => ({
-        data: { repository: this.id, ...issue },
-        component: new (this.resource === 'issues' ? IssueComponent : PullRequestComponent)(
-          issue.id as string,
-          `issue_${issue.number}`
-        ),
-        details: { hasNextPage: true },
-        assignees: { hasNextPage: true },
-        labels: { hasNextPage: true },
-        participants: { hasNextPage: true },
-        timeline: { hasNextPage: true }
-      }));
+        this.issues.items = get(data, `${this.resource}.nodes`, []).map((issue: TObject) => ({
+          data: { repository: this.id, ...issue },
+          component: new (this.resource === 'issues' ? IssueComponent : PullRequestComponent)(
+            issue.id as string,
+            `issue_${issue.number}`
+          ),
+          details: { hasNextPage: true },
+          assignees: { hasNextPage: true },
+          labels: { hasNextPage: true },
+          participants: { hasNextPage: true },
+          timeline: { hasNextPage: true }
+        }));
 
-      const pageInfo = get(data, `${this.resource}.page_info`, {});
-      this.issues.hasNextPage = pageInfo.has_next_page ?? false;
-      this.issues.endCursor = pageInfo.end_cursor ?? this.issues.endCursor;
+        const pageInfo = get(data, `${this.resource}.page_info`, {});
+        this.issues.hasNextPage = pageInfo.has_next_page ?? false;
+        this.issues.endCursor = pageInfo.end_cursor ?? this.issues.endCursor;
 
-      if (this.issues.items.length) return;
-    }
+        if (this.issues.items.length > 0) return;
+        else break;
+      }
 
-    const pendingIssues = this.getPendingIssues();
-    if (pendingIssues.length > 0) {
-      pendingIssues.forEach((item) => {
-        const data = response[item.component.alias];
-        const omitFields = ['assignees', 'labels', 'participants', 'timeline'];
+      case Stages.GET_ISSUES_DETAILS: {
+        this.pendingIssues.forEach((item) => {
+          const data = response[item.component.alias];
+          const omitFields = ['assignees', 'labels', 'participants', 'timeline'];
 
-        if (item.details.hasNextPage) {
-          item.details.hasNextPage = false;
-          item.data = { ...item.data, ...omit(data as TObject, omitFields) };
-          item.data.assignees = [];
-          item.data.labels = [];
-          item.data.participants = [];
-          item.data.timeline = [];
-        }
+          if (item.details.hasNextPage) {
+            item.details.hasNextPage = false;
+            item.data = { ...item.data, ...omit(data as TObject, omitFields) };
+            item.data.assignees = [];
+            item.data.labels = [];
+            item.data.participants = [];
+            item.data.timeline = [];
+          }
 
-        const aPageInfo = get(data, 'assignees.page_info', {});
-        (item.data.assignees as []).push(...(get(data, 'assignees.nodes', []) as []));
-        item.assignees.hasNextPage = aPageInfo.has_next_page ?? false;
-        item.assignees.endCursor = aPageInfo.end_cursor ?? item.assignees.endCursor;
+          const aPageInfo = get(data, 'assignees.page_info', {});
+          (item.data.assignees as []).push(...(get(data, 'assignees.nodes', []) as []));
+          item.assignees.hasNextPage = aPageInfo.has_next_page ?? false;
+          item.assignees.endCursor = aPageInfo.end_cursor ?? item.assignees.endCursor;
 
-        const lPageInfo = get(data, 'labels.page_info', {});
-        (item.data.labels as []).push(...(get(data, 'labels.nodes', []) as []));
-        item.labels.hasNextPage = lPageInfo.has_next_page ?? false;
-        item.labels.endCursor = lPageInfo.end_cursor ?? item.labels.endCursor;
+          const lPageInfo = get(data, 'labels.page_info', {});
+          (item.data.labels as []).push(...(get(data, 'labels.nodes', []) as []));
+          item.labels.hasNextPage = lPageInfo.has_next_page ?? false;
+          item.labels.endCursor = lPageInfo.end_cursor ?? item.labels.endCursor;
 
-        const pPageInfo = get(data, 'participants.page_info', {});
-        (item.data.participants as []).push(...(get(data, 'participants.nodes', []) as []));
-        item.participants.hasNextPage = pPageInfo.has_next_page ?? false;
-        item.participants.endCursor = pPageInfo.end_cursor ?? item.participants.endCursor;
+          const pPageInfo = get(data, 'participants.page_info', {});
+          (item.data.participants as []).push(...(get(data, 'participants.nodes', []) as []));
+          item.participants.hasNextPage = pPageInfo.has_next_page ?? false;
+          item.participants.endCursor = pPageInfo.end_cursor ?? item.participants.endCursor;
 
-        const tPageInfo = get(data, 'timeline.page_info', {});
-        (item.data.timeline as []).push(...(get(data, 'timeline.nodes', []) as []));
-        item.timeline.hasNextPage = tPageInfo.has_next_page ?? false;
-        item.timeline.endCursor = tPageInfo.end_cursor ?? item.timeline.endCursor;
-      });
+          const tPageInfo = get(data, 'timeline.page_info', {});
+          (item.data.timeline as []).push(...(get(data, 'timeline.nodes', []) as []));
+          item.timeline.hasNextPage = tPageInfo.has_next_page ?? false;
+          item.timeline.endCursor = tPageInfo.end_cursor ?? item.timeline.endCursor;
+        });
 
-      if (this.getPendingIssues().length > 0) return;
-    }
+        if (this.pendingIssues.length > 0) return;
 
-    if (this.getPendingIssues().length === 0 && !this.reactions) {
-      const reactables: TObject[] = this.issues.items
-        .filter((issue) => issue.data.reaction_groups)
-        .map((issue) => ({ repository: this.id, issue: issue.data.id }));
+        const reactables: TObject[] = this.issues.items
+          .filter((issue) => issue.data.reaction_groups)
+          .map((issue) => ({ repository: this.id, issue: issue.data.id }));
 
-      reactables.push(
-        ...this.issues.items.reduce((eReactables: TObject[], issue) => {
-          if (!issue.data.timeline) return eReactables;
-          return eReactables.concat(
-            (issue.data.timeline as [])
-              .filter((event: TObject) => event.reaction_groups)
-              .map((event: TObject) => ({
-                repository: this.id,
-                issue: issue.data.id,
-                event: event.id
-              }))
-          );
-        }, [])
-      );
-
-      this.reactions = reactables.map((reactable, index) => ({
-        reactable,
-        component: new ReactionComponent(
-          (reactable.event || reactable.issue) as string,
-          `issue_reactable_${index}`
-        ),
-        reactions: [],
-        hasNextPage: true
-      }));
-
-      return;
-    }
-
-    const pendindReactables = this.getPendingReactables();
-    if (pendindReactables.length > 0) {
-      pendindReactables.forEach((meta) => {
-        const data = response[meta.component.alias];
-        meta.reactions.push(...get(data, 'reactions.nodes', []));
-        meta.hasNextPage = get(data, 'reactions.page_info.has_next_page', false);
-        meta.endCursor = get(data, 'reactions.page_info.end_cursor', meta.endCursor);
-      });
-
-      if (this.getPendingReactables().length > 0) return;
-    }
-
-    if (this.getPendingIssues().length === 0 && this.getPendingReactables().length === 0) {
-      const issues = this.issues.items.map((issue) => {
-        if (issue.error) issue.data._metadata = { error: JSON.stringify(issue.error) };
-        else issue.data._metadata = { updatedAt: new Date(), endCursor: issue.timeline.endCursor };
-
-        return compact(omit(issue.data, 'timeline')) as TObject;
-      });
-
-      const timeline = this.issues.items.reduce((acc: TObject[], issue) => {
-        if (!issue.data.timeline) return acc;
-        return acc.concat(
-          (issue.data.timeline as TObject[]).map(
-            (tl) =>
-              compact({
-                repository: this.id,
-                issue: issue.data.id,
-                ...tl
-              }) as TObject
-          )
+        reactables.push(
+          ...this.issues.items.reduce((eReactables: TObject[], issue) => {
+            if (!issue.data.timeline) return eReactables;
+            return eReactables.concat(
+              (issue.data.timeline as [])
+                .filter((event: TObject) => event.reaction_groups)
+                .map((event: TObject) => ({
+                  repository: this.id,
+                  issue: issue.data.id,
+                  event: event.id
+                }))
+            );
+          }, [])
         );
-      }, []);
 
-      const reactions = (this.reactions || []).reduce(
-        (reactions: TObject[], meta) =>
-          reactions.concat(meta.reactions.map((reaction) => ({ ...meta.reactable, ...reaction }))),
-        []
-      );
+        this.reactions = reactables.map((reactable, index) => ({
+          reactable,
+          component: new ReactionComponent(
+            (reactable.event || reactable.issue) as string,
+            `issue_reactable_${index}`
+          ),
+          reactions: [],
+          hasNextPage: true
+        }));
 
-      await Promise.all([
-        (this.resource === 'issues' ? Issue : PullRequest).upsert(issues, session),
-        TimelineEvent.upsert(timeline, session),
-        Reaction.upsert(reactions, session)
-      ]);
+        if (this.reactions.length > 0) return;
+        else break;
+      }
 
-      await Repository.collection.updateOne(
-        { _id: this.meta.id },
-        { $set: { [`_metadata.${this.meta.resource}.endCursor`]: this.issues.endCursor } },
-        { session }
-      );
+      case Stages.GET_REACTIONS: {
+        this.pendingReactables.forEach((meta) => {
+          const data = response[meta.component.alias];
+          meta.reactions.push(...get(data, 'reactions.nodes', []));
+          meta.hasNextPage = get(data, 'reactions.page_info.has_next_page', false);
+          meta.endCursor = get(data, 'reactions.page_info.end_cursor', meta.endCursor);
+        });
 
-      this.issues.items = [];
-      this.reactions = undefined;
+        if (this.pendingReactables.length > 0) return;
+        else break;
+      }
+
+      default:
+        throw new ResourceUpdateError('Unknown condition reached!');
     }
+
+    const issues = this.issues.items.map((issue) => {
+      if (issue.error) issue.data._metadata = { error: JSON.stringify(issue.error) };
+      else issue.data._metadata = { updatedAt: new Date(), endCursor: issue.timeline.endCursor };
+
+      return compact(omit(issue.data, 'timeline')) as TObject;
+    });
+
+    const timeline = this.issues.items.reduce((acc: TObject[], issue) => {
+      if (!issue.data.timeline) return acc;
+      return acc.concat(
+        (issue.data.timeline as TObject[]).map(
+          (tl) =>
+            compact({
+              repository: this.id,
+              issue: issue.data.id,
+              ...tl
+            }) as TObject
+        )
+      );
+    }, []);
+
+    const reactions = (this.reactions || []).reduce(
+      (reactions: TObject[], meta) =>
+        reactions.concat(meta.reactions.map((reaction) => ({ ...meta.reactable, ...reaction }))),
+      []
+    );
+
+    await Promise.all([
+      (this.resource === 'issues' ? Issue : PullRequest).upsert(issues, session),
+      TimelineEvent.upsert(timeline, session),
+      Reaction.upsert(reactions, session)
+    ]);
+
+    await Repository.collection.updateOne(
+      { _id: this.meta.id },
+      { $set: { [`_metadata.${this.meta.resource}.endCursor`]: this.issues.endCursor } },
+      { session }
+    );
+
+    this.issues.items = [];
+    this.reactions = [];
 
     if (this.isDone()) {
       await Repository.collection.updateOne(
@@ -298,22 +311,21 @@ export default class RepositoryIssuesHander extends AbstractRepositoryHandler {
 
   async error(err: Error): Promise<void> {
     if (err instanceof RetryableError || err instanceof ForbiddenError) {
-      const hasNextDetails = this.getPendingIssues().length > 0;
-      const hasNextReactions = this.getPendingReactables().length > 0;
+      const hasNextDetails = this.pendingIssues.length > 0;
+      const hasNextReactions = this.pendingReactables.length > 0;
 
-      if (this.batchSize > 1)
-        if (this.batchSize > 1) {
-          this.batchSize = Math.floor(this.batchSize / 2);
-          return;
-        }
+      if (this.batchSize > 1) {
+        this.batchSize = Math.floor(this.batchSize / 2);
+        return;
+      }
 
       if (hasNextDetails) {
-        const issue = this.getPendingIssues()[0];
+        const issue = this.pendingIssues[0];
         issue.details.hasNextPage = issue.assignees.hasNextPage = issue.labels.hasNextPage = issue.participants.hasNextPage = issue.timeline.hasNextPage = false;
         issue.error = err;
       } else if (hasNextReactions) {
         // TODO -- handle single reactables problems
-        const reaction = this.getPendingReactables()[0];
+        const reaction = this.pendingReactables[0];
         reaction.hasNextPage = false;
         reaction.error = err;
       }
@@ -324,11 +336,12 @@ export default class RepositoryIssuesHander extends AbstractRepositoryHandler {
     throw new ResourceUpdateError(err.message, err);
   }
 
-  getPendingIssues(): TIssueMetadata[] {
+  get pendingIssues(): TIssueMetadata[] {
     return this.issues.items
       .filter(
         (i) =>
           i.details.hasNextPage ||
+          i.assignees.hasNextPage ||
           i.labels.hasNextPage ||
           i.participants.hasNextPage ||
           i.timeline.hasNextPage
@@ -336,22 +349,20 @@ export default class RepositoryIssuesHander extends AbstractRepositoryHandler {
       .slice(0, this.batchSize);
   }
 
-  getPendingReactables(): TReactableMetadata[] {
+  get pendingReactables(): TReactableMetadata[] {
     return this.reactions?.filter((rm) => rm.hasNextPage).slice(0, this.batchSize) || [];
   }
 
   get alias(): string[] {
-    const alias = this.getPendingIssues().map((meta) => meta.component.alias);
+    const alias = this.pendingIssues.map((meta) => meta.component.alias);
     alias.push(...(super.alias === 'string' ? [super.alias] : super.alias));
-    alias.push(...this.getPendingReactables().map((meta) => meta.component.alias));
+    alias.push(...this.pendingReactables.map((meta) => meta.component.alias));
     return alias;
   }
 
   hasNextPage(): boolean {
     return (
-      this.issues.hasNextPage ||
-      this.getPendingIssues().length > 0 ||
-      this.getPendingReactables().length > 0
+      this.issues.hasNextPage || this.pendingIssues.length > 0 || this.pendingReactables.length > 0
     );
   }
 }
