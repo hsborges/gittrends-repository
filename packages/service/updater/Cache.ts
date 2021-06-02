7bfb08663e6c25168ff00d4736101f371c43013f
Hudson Silva Borges <hudsonsilbor@gmail.com>
2021-06-02 16:05:25 -0400

    fix(service): replace object-hash due to performance issues

--- packages/service/updater/Cache.ts
+++ packages/service/updater/Cache.ts
@@ -2,10 +2,11 @@
  *  Author: Hudson S. Borges
  */
 import LfuSet from 'collections/lfu-set';
-import { MD5 } from 'object-hash';
+import hasher from 'node-object-hash';
 
 export default class UpdaterCache {
   readonly cache: LfuSet<string>;
+  readonly hashSortCoerce = hasher({ sort: true, coerce: true });
 
   constructor(cacheSize: number) {
     this.cache = new LfuSet<string>([], cacheSize);
@@ -13,11 +14,11 @@ export default class UpdaterCache {
 
   add(object: TObject | TObject[]): void {
     (Array.isArray(object) ? object : [object]).forEach((obj) =>
-      this.cache.add(MD5(obj.id ? { id: obj.id } : obj))
+      this.cache.add(this.hashSortCoerce.hash(obj.id ? { id: obj.id } : obj))
     );
   }
 
   has(object: TObject): boolean {
-    return this.cache.has(MD5(object.id ? { id: object.id } : object));
+    return this.cache.has(this.hashSortCoerce.hash(object.id ? { id: object.id } : object));
   }
 }
