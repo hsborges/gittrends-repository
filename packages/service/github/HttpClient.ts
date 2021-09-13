07d3db5b22f69673cded5145e6772859b4eec20a
Hudson Silva Borges <hudsonsilbor@gmail.com>
2021-09-13 13:50:15 -0400

    fix(service): retry only failed requests without status code empty or equal to 6xx

--- packages/service/github/HttpClient.ts
+++ packages/service/github/HttpClient.ts
@@ -38,59 +38,66 @@ export default async function (query: TObject): Promise<AxiosResponse> {
       const source = axios.CancelToken.source();
 
       const timeout = setTimeout(() => {
-        source.cancel(
-          `Cancelled request, took longer than ${(TIMEOUT * 2) / 1000}s to get complete response.`
-        );
-      }, TIMEOUT * 2);
+        source.cancel(`Cancelled request. Took longer than ${TIMEOUT}ms to get complete response.`);
+      }, TIMEOUT);
 
       return requestClient({ method: 'post', data: query, cancelToken: source.token })
         .catch((err) => {
-          switch (err.response?.status) {
-            case 408:
-              throw new Errors.TimedoutError(err.message, err, err.response?.data);
-            case 500:
-              throw new Errors.InternalServerError(err.message, err, err.response?.data);
-            case 502:
-              throw new Errors.BadGatewayError(err.message, err, err.response?.data);
-            default:
-              throw new Errors.RequestError(err.message, err, err.response?.data, query);
-          }
+          if (!err.response?.status || /^6\d{2}$/.test(err.response?.status)) throw err;
+          throw new pRetry.AbortError(err);
         })
+
         .finally(() => clearTimeout(timeout));
     },
     { retries: RETRIES, minTimeout: 100, maxTimeout: 500, randomize: true }
-  ).then((response) => {
-    const data = normalize(compact(response.data));
+  )
+    .catch((err) => {
+      switch (err.response?.status) {
+        case 408:
+          throw new Errors.TimedoutError(err.message, err, err.response?.data);
+        case 500:
+          throw new Errors.InternalServerError(err.message, err, err.response?.data);
+        case 502:
+          throw new Errors.BadGatewayError(err.message, err, err.response?.data);
+        default: {
+          throw new Errors.RequestError(err.message, err, err.response?.data, query);
+        }
+      }
+    })
+    .then((response) => {
+      const data = normalize(compact(response.data));
 
-    if (data?.errors?.length) {
-      const message = `Response errors (${data.errors.length}): ${JSON.stringify(data.errors)}`;
-      if (data.errors.find((e: TObject) => e.type === 'FORBIDDEN')) {
-        throw new Errors.ForbiddenError(message, null, data, query);
+      if (data?.errors?.length) {
+        const message = `Response errors (${data.errors.length}): ${JSON.stringify(data.errors)}`;
+        if (data.errors.find((e: TObject) => e.type === 'FORBIDDEN')) {
+          throw new Errors.ForbiddenError(message, null, data, query);
+        }
+        if (data.errors.find((e: TObject) => e.type === 'INTERNAL')) {
+          throw new Errors.InternalError(message, null, data, query);
+        }
+        if (data.errors.find((e: TObject) => e.type === 'NOT_FOUND')) {
+          throw new Errors.NotFoundError(message, null, data, query);
+        }
+        if (data.errors.find((e: TObject) => e.type === 'MAX_NODE_LIMIT_EXCEEDED')) {
+          throw new Errors.MaxNodeLimitExceededError(message, null, data, query);
+        }
+        if (data.errors.find((e: TObject) => e.type === 'SERVICE_UNAVAILABLE')) {
+          throw new Errors.ServiceUnavailableError(message, null, data, query);
+        }
+        if (data.errors.find((e: TObject) => e.message === 'timedout')) {
+          throw new Errors.TimedoutError(message, null, data, query);
+        }
+        if (data.errors.find((e: TObject) => e.message === 'loading')) {
+          throw new Errors.LoadingError(message, null, data, query);
+        }
+        if (
+          data.errors.find((e: TObject) => /^something.went.wrong.+/i.test(e.message as string))
+        ) {
+          throw new Errors.SomethingWentWrongError(message, null, data, query);
+        }
+        throw new Errors.RequestError(message, null, data, query);
       }
-      if (data.errors.find((e: TObject) => e.type === 'INTERNAL')) {
-        throw new Errors.InternalError(message, null, data, query);
-      }
-      if (data.errors.find((e: TObject) => e.type === 'NOT_FOUND')) {
-        throw new Errors.NotFoundError(message, null, data, query);
-      }
-      if (data.errors.find((e: TObject) => e.type === 'MAX_NODE_LIMIT_EXCEEDED')) {
-        throw new Errors.MaxNodeLimitExceededError(message, null, data, query);
-      }
-      if (data.errors.find((e: TObject) => e.type === 'SERVICE_UNAVAILABLE')) {
-        throw new Errors.ServiceUnavailableError(message, null, data, query);
-      }
-      if (data.errors.find((e: TObject) => e.message === 'timedout')) {
-        throw new Errors.TimedoutError(message, null, data, query);
-      }
-      if (data.errors.find((e: TObject) => e.message === 'loading')) {
-        throw new Errors.LoadingError(message, null, data, query);
-      }
-      if (data.errors.find((e: TObject) => /^something.went.wrong.+/i.test(e.message as string))) {
-        throw new Errors.SomethingWentWrongError(message, null, data, query);
-      }
-      throw new Errors.RequestError(message, null, data, query);
-    }
 
-    return { ...response, data };
-  });
+      return { ...response, data };
+    });
 }
