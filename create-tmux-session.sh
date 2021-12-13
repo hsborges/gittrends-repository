aaf0ca69b78e6023204f41413157660173d74ac4
Hudson S. Borges <hudsonsilbor@gmail.com>
2021-12-13 19:13:18 -0400

    fix(tmux): fix tmux script

--- create-tmux-session.sh
+++ create-tmux-session.sh
@@ -1,14 +1,16 @@
+Cols=$(tput cols)
+
 tmux new-session \; \
-  send-keys 'docker-compose logs -f --tail 25 updater users-updater scheduler' C-m  \; \
+  send-keys 'yarn workspace @gittrends/service pm2 logs "/repos|users|scheduler/"' Enter  \; \
+  split-window -v \; \
+  send-keys 'yarn workspace @gittrends/service pm2 logs --raw proxy-server' Enter  \; \
   split-window -h \; \
-  select-pane -t 0 \; \
-  split-window -v \; \
-  resize-pane -y 15 \; \
-  send-keys 'docker-compose logs -f --tail 25 proxy-server' C-m  \; \
-  select-pane -t 2 \; \
-  send-keys 'htop' C-m  \; \
-  split-window -v \; \
-  send-keys 'docker-compose exec mongo bash -c "mongostat -i --username \$MONGO_INITDB_ROOT_USERNAME --authenticationDatabase admin --authenticationMechanism SCRAM-SHA-1 --password \$MONGO_INITDB_ROOT_PASSWORD"' C-m  \; \
-  split-window \; \
-  send-keys 'docker-compose exec mongo bash -c "mongotop --username \$MONGO_INITDB_ROOT_USERNAME --authenticationDatabase admin --authenticationMechanism SCRAM-SHA-1 --password \$MONGO_INITDB_ROOT_PASSWORD"' C-m  \; \
-  resize-pane -t 0 -x 115 \;
+  send-keys 'htop' Enter  \; \
+  split-window -h \; \
+  send-keys 'docker-compose exec mongo bash -c "mongostat -i --username \$MONGO_INITDB_ROOT_USERNAME --authenticationDatabase admin --authenticationMechanism SCRAM-SHA-1 --password \$MONGO_INITDB_ROOT_PASSWORD"' Enter  \; \
+  resize-pane -t 1 -x $(($Cols * 35 / 100)) \; \
+  resize-pane -t 2 -x $(($Cols * 35 / 100)) \; \
+  resize-pane -t 3 -x $(($Cols * 30 / 100)) \; \
+  new-window \; \
+  send-keys 'yarn workspace @gittrends/service pm2 monit' Enter  \; \
+  select-window -t 0 \;
