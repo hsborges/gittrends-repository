76b46bdabfe9aa0ffb21ecc40b1fe7df332e42b6
Hudson S. Borges <hudsonsilbor@gmail.com>
2021-12-19 21:30:57 -0400

    fix(service): set db field instead of database

--- packages/service/redis.ts
+++ packages/service/redis.ts
@@ -1,4 +1,6 @@
 import { parseRedisUrl } from 'parse-redis-url-simple';
 
 export const REDIS_URL = process.env.GT_REDIS_URL || 'redis://localhost:6379/0';
-export const REDIS_PROPS = parseRedisUrl(REDIS_URL)[0];
+
+const { host, port, database, password } = parseRedisUrl(REDIS_URL)[0];
+export const REDIS_PROPS = { host, port, password, db: database };
