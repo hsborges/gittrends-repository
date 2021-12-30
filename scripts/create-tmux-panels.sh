e09b0788a1e17f11f0ded3cb64b4ca6538ff386d
Hudson Silva Borges <hudsonsilbor@gmail.com>
2021-12-30 18:03:37 -0400

    refactor(tmux): increase sleep time on tmux script

--- scripts/create-tmux-panels.sh
+++ scripts/create-tmux-panels.sh
@@ -4,9 +4,9 @@ COLS_COUNT=$(tput cols)
 tmux new-session -s 'gittrends-panels' -n 'pm2-runtime' \; \
   send-keys 'yarn workspace @gittrends/service pm2-runtime service.config.js' C-m \; \
   new-window -n 'pm2-logs' \; \
-  send-keys 'bash -c "sleep 2 && yarn workspace @gittrends/service pm2 logs --raw \"/repos|users|scheduler/\""' C-m \; \
+  send-keys 'bash -c "sleep 15 && yarn workspace @gittrends/service pm2 logs --raw \"/repos|users|scheduler/\""' C-m \; \
   split-window -v \; \
-  send-keys 'bash -c "sleep 4 && yarn workspace @gittrends/service pm2 logs --raw proxy-server"' C-m \; \
+  send-keys 'bash -c "sleep 15 && yarn workspace @gittrends/service pm2 logs --raw proxy-server"' C-m \; \
   split-window -h \; \
   send-keys 'htop' C-m \; \
   split-window -h \; \
@@ -15,5 +15,5 @@ tmux new-session -s 'gittrends-panels' -n 'pm2-runtime' \; \
   resize-pane -t 2 -x $(($COLS_COUNT * 35 / 100)) \; \
   resize-pane -t 3 -x $(($COLS_COUNT * 30 / 100)) \; \
   new-window -n 'pm2-monit' \; \
-  send-keys 'bash -c "sleep 6 && yarn workspace @gittrends/service pm2 monit"' C-m \; \
+  send-keys 'bash -c "sleep 15 && yarn workspace @gittrends/service pm2 monit"' C-m \; \
   select-window -t 1 \;
