a4f8b14d4653c4a6722fb7103ec1b2856658f266
Hudson Silva Borges <hudsonsilbor@gmail.com>
2022-01-10 23:54:36 -0400

    fix(service): fix helper

--- packages/service/helpers/proxy-http-client.ts
+++ packages/service/helpers/proxy-http-client.ts
@@ -6,7 +6,7 @@ import HttpClient from '../github/HttpClient';
 const proxyUrl = new URL(process.env.GT_PROXY_URL || 'http://localhost:3000');
 
 const httpClient = new HttpClient({
-  protocol: proxyUrl.protocol,
+  protocol: proxyUrl.protocol.replace(/(.*):$/g, '$1'),
   host: proxyUrl.hostname,
   port: proxyUrl.port ? parseInt(proxyUrl.port, 10) : undefined,
   timeout: process.env.GT_PROXY_TIMEOUT ? parseInt(process.env.GT_PROXY_TIMEOUT, 10) : undefined,
