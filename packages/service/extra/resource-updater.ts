832f991042a8071f960f8545150bd241d0c70c8b
Hudson Silva Borges <hudsonsilbor@gmail.com>
2022-01-25 16:39:12 -0400

    feat(service): implements single users update

--- packages/service/extra/resource-updater.ts
+++ packages/service/extra/resource-updater.ts
@@ -3,11 +3,32 @@
  */
 import { program, Option, Argument } from 'commander';
 
-import mongoClient, { MongoRepository, Repository } from '@gittrends/database';
+import mongoClient, { Actor, MongoRepository, Repository } from '@gittrends/database';
 
 import httpClient from '../helpers/proxy-http-client';
 import { config } from '../package.json';
+import { ActorsUpdater } from '../updater/ActorUpdater';
 import { RepositoryUpdater } from '../updater/RepositoryUpdater';
+
+async function updateRepositoryResource(resourceId: string, resource: string): Promise<void> {
+  await MongoRepository.get(Repository)
+    .collection.findOne({
+      $or: [{ _id: resourceId }, { name_with_owner: new RegExp(resourceId, 'i') }]
+    })
+    .then((repo) => {
+      if (!repo) throw new Error('Repository not found!');
+      return new RepositoryUpdater(repo._id.toString(), [resource as any], httpClient).update();
+    });
+}
+
+async function updateActor(resourceId: string): Promise<void> {
+  await MongoRepository.get(Actor)
+    .collection.findOne({ _id: resourceId })
+    .then((user) => {
+      if (!user) throw new Error('Actor not found!');
+      return new ActorsUpdater(user._id.toString(), httpClient).update();
+    });
+}
 
 /* execute */
 program
@@ -15,8 +36,8 @@ program
   .addOption(
     new Option('-r, --resource [string]').choices(config.resources).default(config.resources[0])
   )
-  .addArgument(new Argument('<repository>').argRequired())
-  .action(async (repositoryId: string): Promise<void> => {
+  .addArgument(new Argument('<repository_or_actor>').argRequired())
+  .action(async (resourceId: string): Promise<void> => {
     const opts = program.opts();
 
     if (config.resources.map((r) => r.toLowerCase()).indexOf(opts.resource) < 0)
@@ -24,15 +45,9 @@ program
 
     await mongoClient
       .connect()
-      .then(() =>
-        MongoRepository.get(Repository).collection.findOne({
-          $or: [{ _id: repositoryId }, { name_with_owner: new RegExp(repositoryId, 'i') }]
-        })
-      )
-      .then((repo) => {
-        if (!repo) throw new Error('Repository not found!');
-
-        return new RepositoryUpdater(repo._id.toString(), [opts.resource], httpClient).update();
+      .then(() => {
+        if (opts.resource === 'users') return updateActor(resourceId);
+        return updateRepositoryResource(resourceId, opts.resource);
       })
       .finally(() => mongoClient.close());
   })
